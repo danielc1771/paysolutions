07aa79289370152ce65700958b27f9b130753bfb
addictedsalas <ssalas.wt@gmail.com>
2025-11-18 15:36:44 -0500

    added notes to borrower profiles

--- supabase_rls_borrower_notes.sql
+++
@@ -1,5 +0,0 @@
--- Grant permissions on borrower_notes table
-GRANT ALL ON borrower_notes TO postgres;
-GRANT ALL ON borrower_notes TO authenticated;
-GRANT ALL ON borrower_notes TO service_role;
-GRANT ALL ON borrower_notes TO anon;
